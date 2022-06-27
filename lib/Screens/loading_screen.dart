aaac0cc6469cdafd831d97a3235727449ae2934e
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-06-27 18:46:25 +0530

    fix : Update loading_screen.dart

--- lib/Screens/loading_screen.dart
+++ lib/Screens/loading_screen.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:clima_weather_app/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  Future<void> geoLocation() async {
+    Location location = Location();
+    await location.getLocation();
+    print('Latitude : ${location.latitude}');
+    print('Longitude : ${location.longitude}');
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+        child:
+              RaisedButton(
+                 onPressed: () {
+                 //Get the current location
+                 geoLocation();
+               }, child: Text('Get Location'),
+              ),
       ),
     );
   }
