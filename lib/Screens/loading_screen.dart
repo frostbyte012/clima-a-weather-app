230d86b6786db39a0adcc88b71cb0d2e814c1f33
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-06-30 17:04:22 +0530

    Add : loading_screens.dart

--- lib/Screens/loading_screen.dart
+++ lib/Screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:clima_weather_app/services/location.dart';
+import 'package:clima_weather_app/services/network.dart';
+import 'package:clima_weather_app/Screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
 
@@ -9,25 +11,35 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  Future<void> geoLocation() async {
-    Location location = Location();
-    await location.getLocation();
-    print('Latitude : ${location.latitude}');
-    print('Longitude : ${location.longitude}');
+
+  GetNetworkLocationData getNetworkLocationData = new GetNetworkLocationData();
+
+
+  @override
+  void initState() {
+    super.initState();
+    getNetworkLocationData.getResponse()
+        .whenComplete(() => Navigator.push(context, MaterialPageRoute(builder: (context)=>LocationScreen(data: getNetworkLocationData.data,))));
+
+
   }
+
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child:
-              RaisedButton(
-                 onPressed: () {
-                 //Get the current location
-                 geoLocation();
-               }, child: Text('Get Location'),
+        child:SpinKitFadingCircle(
+          size: 100.0,
+          itemBuilder: (BuildContext context, int index) {
+            return DecoratedBox(
+              decoration: BoxDecoration(
+                color: index.isEven ? Colors.red : Colors.green,
               ),
+            );
+          },
+        )
       ),
     );
   }
